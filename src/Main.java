0548568d63ddb0bbda75e12d127428db6ad28351
krzys <krzysztofmuller1@gmail.com>
2021-11-14 21:11:03 +0100

    cw_4

--- src/Main.java
+++ src/Main.java
@@ -4,48 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* przykład 4. Blok try catch */
+        /* zad.1
+        a) stworzyć tablicę 6-cio elementową, uzupełnić przykładowymi wartościami,
+        b) wprowadzić z konsoli numer indexu tablicy który chcemy wyświetlić, użyć metody next() --> scan.next();
+        c) zabezpieczyć kod przed wprowadzeniem:
+           - za dużej liczby (index > tab.length),
+           - wprowadzeniem innego znaku niż liczba,
+        d) użyć metody finally aby wyświetlić komunikat zakończenia programu
 
-        String tab[] = {"Ala", "Ola", "Ela", "Ula"};
-        Scanner scan = new Scanner(System.in);
-        int index = -1;
+           PODPOWIEDZI:
+           scan.next() - można wprowadzać dowolny znak z klawiatury,
+           index = Integer.parseInt(scan.next()); - parsowanie zmiennej do typu int (liczbowego calkowitego)
+        */
 
-        try {
-            index = Integer.parseInt(scan.next());
-        } catch (Exception n) {
-            System.out.println("Błąd, brak informacji jaki to błąd");
-        }
-
-        System.out.println("Podaj indeks tablicy [0-3], który ma być wyświetlony: ");
-
-        /* W bloku try sprawdzamy poprawność odebranych znaków z klawiatury */
-        try {
-            index = Integer.parseInt(scan.next());
-            /* block catch przechwytuje błąd, jeśli w tym przypadku znak jest różny od numeru,
-             * porównania dokonujemy z typem 'NumberFormatException' */
-        } catch (NumberFormatException e) {
-            System.out.println("Błąd, wprowadzony znak nie jest cyfrą!");
-            throw e;
-        }
-
-        if (index < 3 && index >= 0) {
-            try {
-                System.out.println(tab[index]);
-                /* sprawdzenie czy przepełniona tablicca 'ArrayIndexOutOfBoundsException' */
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Błąd Podałeś index: " + index + ", a dozwolone są od 0 do " + (tab.length - 1));
-            } finally {
-                System.out.println("Koniec programu");
-            }
-        }
-
-        /* Przykład null pointer */
-        try {
-            String name = null;
-            name.toString();
-        } catch (NullPointerException e) {
-            System.out.println("Błąd nie można wywołać metody toString() na null");
-            throw e; // rzuca komunikat z klasy Exception
-        }
     }
 }
