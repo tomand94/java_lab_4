fc040b20a2441088b974d73913d18e9781aa61a2
krzys <krzysztofmuller1@gmail.com>
2021-11-14 22:16:17 +0100

    ćw 4

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         /* zad.1
-        a) stworzyć tablicę 6-cio elementową, uzupełnić przykładowymi wartościami,
+        a) stworzyć tablicę 6-cio elementową, uzupełnić przykładowymi wartościami (dowolny typ tablicy),
         b) wprowadzić z konsoli numer indexu tablicy który chcemy wyświetlić, użyć metody next() --> scan.next();
         c) zabezpieczyć kod przed wprowadzeniem:
            - za dużej liczby (index > tab.length),
            - wprowadzeniem innego znaku niż liczba,
         d) użyć metody finally aby wyświetlić komunikat zakończenia programu
+        e) wprowadzanie indeksu niech będzie zapetlone do momentu wprowadzenia poprawnego indeksu
 
            PODPOWIEDZI:
            scan.next() - można wprowadzać dowolny znak z klawiatury,
            index = Integer.parseInt(scan.next()); - parsowanie zmiennej do typu int (liczbowego calkowitego)
+
+           warto stworzyć flagę np. isIndexCorrect = false, i niech pętla sie wykonuje, dopuki flaga jest false,
+           boolean isIndexCorrect
+           white(!isIndexCorrect) {}
         */
+
+
+
+        /* zad.2
+        a) stworzyć dowolny przykład z NullPonterException,
+        b) stworzyć dowolny przykład z ArithmeticException ,
+        c) stworzyć dowolny przykład z 2 lub 3 blokami catch i z blokiem finally,
+         */
 
     }
 }
